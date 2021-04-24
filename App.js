113a39ee066ec48a461ae9fa0b5a97ddce510f02
heejuShin <21800412@handong.edu>
2021-04-24 21:53:44 +0900

    latitude and longitude

--- App.js
+++ App.js
@@ -1,59 +1,101 @@
-import React, {Component, useState } from 'react';
-import {Text, View, Button, StyleSheet, Image, Switch} from 'react-native';
+import Loading from  "./Loading";
+import * as Location from "expo-location";
+import * as TaskManager from 'expo-task-manager';
+import * as BackgroundFetch from 'expo-background-fetch';
+import React, {Component, useState, useEffect } from 'react';
+import {Text, View, Button, StyleSheet, Image, Switch, Alert} from 'react-native';
 
-class App extends Component{
+const LOCATION_TRACKING = 'location-tracking';
 
+export default class extends React.Component {
   state = {
+    isLoading: true,
     witchValue: false,
     inPlace: false,
-    cnt: 2,
+    cnt: 3,
     latitude: null,
-    longitude: null
+    longitude: null,
+    isLoading: true,
+    placeName: "양덕초등학교",
+    time: 0,
   };
+  watchId = (null: ?number);
   toggleSwitch = value =>{ this.setState({ switchValue: value})};
   inPlaceChange = value =>{ this.setState({ inPlace: value})};
 
-  render(){
-    let placeName = "양덕 초등학교";
-    return (
-      <View style={styles.background}>
-        <View style={styles.active}>
-          <Switch style={{ marginTop: 31 }} onValueChange={this.toggleSwitch} value={this.state.switchValue}/>
-          <Text style={styles.active_text}>{this.state.switchValue ? '어플이 활성화되었습니다!' : '어플을 활성화시켜주세요!'}</Text>
-        </View>
-        {this.state.switchValue && this.state.inPlace ?
-          <View>
-            <View style={styles.alert_place}>
-              <Text style={styles.placeName}>"{placeName}"</Text>
-              <Text style={styles.text}>어린이 보호 구역입니다.</Text>
-            </View>
-            <View style={styles.alert_num}>
-              <View style={{ flex: 1}}>
-                <Image source={require('./images/child.png')} style={styles.img}></Image>
-              </View>
-              <View style={{ flex: 3, paddingLeft: 15,}}>
-                <View style={{flexDirection: "row"}}>
-                  <Text style={styles.text}>현재 </Text><Text style={styles.num}>{this.state.cnt}명</Text><Text style={styles.text}> 감지됩니다. </Text>
-                </View>
-                <View>
-                  <Text style={styles.text}>주의하세요!</Text>
-                </View>
-              </View>
-            </View>
+
+  getLocation = async () => {
+    const { status } = await Location.requestPermissionsAsync();
+
+    try {
+      await Location.requestForegroundPermissionsAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.setState({ isLoading: false });
+      this.setState({ latitude: latitude, longitude: longitude});
+      let location = await Location.watchPositionAsync(
+        {accuracy:Location.Accuracy.High, timeInterval: 5000, distanceInterval: 0},
+        (loc) => {
+          const { isLoading } = this.state;
+          console.log(
+            `${new Date(Date.now()).toLocaleString()}:`+ loc.coords.latitude +" & "+ loc.coords.longitude
+          );
+          this.setState({latitude: loc.coords.latitude, longitude: loc.coords.longitude});
+        }
+      );
+    } catch (error) {
+      //여기서 확인하는 과정도 넣자!
+      Alert.alert("오류", "앱 실행을 위해 위치 정보가 필요합니다. 위치 권한을 설정해주세요.");
+    }
+  };
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading />
+    : (
+        <View style={styles.background}>
+          <View style={styles.active}>
+            <Switch style={{ marginTop: 31 }} onValueChange={this.toggleSwitch} value={this.state.switchValue}/>
+            <Text style={styles.active_text}>{this.state.switchValue ? '어플이 활성화되었습니다!' : '어플을 활성화시켜주세요!'}</Text>
           </View>
-        : <View style={{ flexGlow: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Latitude : {this.state.latitude}</Text>
-            <Text>Longitude: {this.state.longitude}</Text>
-          </View>}
-        <View style={styles.test}>
-          <Switch style={{ marginTop: 31 }} onValueChange={this.inPlaceChange} value={this.state.inPlace}/>
-          <Text style={styles.active_text}>{this.state.inPlace ? '어린이 보호 구역 안' : '어린이 보호 구역 밖'}</Text>
-          <Text style={styles.active_text}>나중에는 사용자의 위치에 따라 변경됨 (현재는 임시로 스위치 사용)</Text>
+          {this.state.switchValue && this.state.inPlace ?
+            <View>
+              <View style={styles.alert_place}>
+                <Text style={styles.placeName}>"{this.state.placeName}"</Text>
+                <Text style={styles.text}>어린이 보호 구역입니다.</Text>
+              </View>
+              <View style={styles.alert_num}>
+                <View style={{ flex: 1}}>
+                  <Image source={require('./images/child.png')} style={styles.img}></Image>
+                </View>
+                <View style={{ flex: 3, paddingLeft: 15,}}>
+                  <View style={{flexDirection: "row"}}>
+                    <Text style={styles.text}>현재 </Text><Text style={styles.num}>{this.state.cnt}명</Text><Text style={styles.text}> 감지됩니다. </Text>
+                  </View>
+                  <View>
+                    <Text style={styles.text}>주의하세요!</Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+          : <View style={{ flexGlow: 1, alignItems: 'center', justifyContent: 'center' }}>
+              <Text>위도 : {this.state.latitude}</Text>
+              <Text>경도 : {this.state.longitude}</Text>
+            </View>}
+          <View style={styles.test}>
+            <Switch style={{ marginTop: 31 }} onValueChange={this.inPlaceChange} value={this.state.inPlace}/>
+            <Text style={styles.active_text}>{this.state.inPlace ? '어린이 보호 구역 안' : '어린이 보호 구역 밖'}</Text>
+            <Text style={styles.active_text}>나중에는 사용자의 위치에 따라 변경됨 (현재는 임시로 스위치 사용)</Text>
+          </View>
         </View>
-      </View>
-    );
+      );
   }
 }
+
 
 const styles = StyleSheet.create({
   test: {
@@ -70,7 +112,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffe896',
   },
   active: {
-    marginTop: 100,
+    marginTop: 70,
     marginBottom : 20,
     width: '100%',
     height: 80,
@@ -121,5 +163,3 @@ const styles = StyleSheet.create({
     //resizeMode: 'contain',
   }
 });
-
-export default App;
---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>로딩 중 입니다</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#ffe896"
+  },
+  text: {
+    color: "#2c2c2c",
+    fontSize: 30
+  }
+});
